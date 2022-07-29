5d36511a301954d4caa94d1efcd612700ba7c251
Raymond Zou <raymond.1zou@gmail.com>
2022-07-28 18:46:19 -0700

    add button functionality

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const display = document.querySelector(".display");
-var displayValue = "";
+display.innerHTML = "&nbsp;"
+var currNum = "";
+var firstOp;
+var secondOp;
+var operator;
 
 function add(a, b) {
     return a + b;
@@ -14,14 +18,62 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operation, a, b) { return operation(a, b); }
-
-function updateDisplay(buttonContent) {
-    displayValue += buttonContent;
-    display.textContent = displayValue;
+function operate(operation, a, b) {
+    a = parseInt(a);
+    b = parseInt(b);
+    var result; 
+    switch (operation) {
+        case "+":
+            result = add(a, b);
+            break;
+        case "-":
+            result = subtract(a, b);
+            break;
+        case "*":
+            result = multiply(a, b);
+            break;
+        case "/":
+            result = divide(a, b);
+            break;
+    }
+    clearDisplay();
+    updateDisplay(result);
 }
 
-const buttons = document.querySelectorAll("button");
-buttons.forEach(button => {
-    button.addEventListener("click", () => updateDisplay(button.textContent));
-})
+function updateDisplay(buttonContent) {
+    currNum += buttonContent;
+    display.textContent = currNum;
+}
+
+function clearDisplay() {
+    currNum = "";
+    display.textContent = currNum;
+    display.innerHTML = "&nbsp;"
+}
+
+function calculate() {
+    secondOp = currNum;
+    operate(operator, firstOp, secondOp);
+}
+
+function saveFirstOp(op) {
+    operator = op;
+    firstOp = currNum;
+    currNum = "";
+}
+
+document.querySelectorAll(".number").
+    forEach(button => {
+        button.addEventListener("click", () => updateDisplay(button.textContent));
+    })
+document.querySelectorAll(".operator").
+    forEach(button => {
+        button.addEventListener("click", () => saveFirstOp(button.textContent));
+    })
+
+document.querySelector(".clear").addEventListener("click", () => clearDisplay());
+document.querySelector(".equal").addEventListener("click", () => calculate());
+
+
+
+
